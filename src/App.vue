8bf85d903498885deeae7ff4572472c6bae5eb61
soupjian <soupjian@163.com>
2021-10-06 22:33:10 +0800

    feat: 首页echarts数据渲染

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"
+    <el-menu :default-active="activeIndex" class="el-menu" mode="horizontal" @select="handleSelect"
       background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
       <el-menu-item index="logo" disabled style="cursor: pointer;">logologologologologo</el-menu-item>
       <el-menu-item index="home">首页</el-menu-item>
@@ -19,7 +19,7 @@
     methods: {
       handleSelect(key) {
         this.$router.push(`/${key}`)
-        this.$store.commit('home/setActiveKey',key)
+        this.$store.commit('home/setActiveKey', key)
       }
     },
     computed: {
@@ -31,10 +31,21 @@
 </script>
 
 <style lang="less">
+  * {
+    box-sizing: border-box;
+  }
+
   html,
   body,
   #app {
+    width: 100vw;
+    height: 100%;
     margin: 0;
     padding: 0;
+  }
+
+  .el-menu {
+    max-height: 61px;
+    overflow: hidden;
   }
 </style>
--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,29 +1,94 @@
 <template>
   <div class="home">
     <div class="title">数据可视化方案平台</div>
-    <div class="sub-title">基于echarts数据可视化</div>
+    <div class="sub-title">基于echarts数据可视化图表库</div>
     <div class="button-link">
       <el-button type="danger" round @click="handleRouterChange('document')"><i
           class="el-icon-document el-icon--left"></i>快速文档</el-button>
       <el-button round plain @click="handleRouterChange('example')"><i class="el-icon-message el-icon--left"></i>所有示例
       </el-button>
     </div>
+    <div id="chart-home" ref="myChart"></div>
+    <div class="footer">soupjian提供技术支持</div>
   </div>
 </template>
 <script>
+  import * as echarts from 'echarts'
   export default {
+    data() {
+      return {
+        myChart: null
+      }
+    },
     methods: {
       handleRouterChange(type) {
         this.$router.push(`/${type}`)
         this.$store.commit('home/setActiveKey', `${type}`)
+      },
+      initChart() {
+        const option = {
+          series: [{
+            name: 'Nightingale Chart',
+            type: 'pie',
+            radius: [50, 200],
+            label: {
+              show: false
+            },
+            center: ['50%', '50%'],
+            roseType: 'radius',
+            itemStyle: {
+              borderRadius: 8
+            },
+            data: [{
+                value: 40,
+                name: 'rose 1'
+              },
+              {
+                value: 38,
+                name: 'rose 2'
+              },
+              {
+                value: 32,
+                name: 'rose 3'
+              },
+              {
+                value: 30,
+                name: 'rose 4'
+              },
+              {
+                value: 28,
+                name: 'rose 5'
+              },
+              {
+                value: 26,
+                name: 'rose 6'
+              },
+              {
+                value: 22,
+                name: 'rose 7'
+              },
+              {
+                value: 18,
+                name: 'rose 8'
+              }
+            ]
+          }]
+        }
+        this.myChart.setOption(option)
       }
     },
+    mounted() {
+      this.myChart = echarts.init(this.$refs.myChart)
+      this.initChart()
+    }
   }
 </script>
 <style lang="less" scoped>
   .home {
+    margin: 0 auto;
     margin-top: 20vh;
-    padding: 0 30px;
+    padding: 0 100px;
+    position: relative;
 
     .title {
       font-size: 55px;
@@ -40,6 +105,22 @@
 
     .button-link {
       margin-top: 40px;
+    }
+
+    #chart-home {
+      height: 400px;
+      width: 400px;
+      top: -30%;
+      position: absolute;
+      right: 50px;
+      z-index: -100;
+    }
+    .footer{
+      position: fixed;
+      bottom: 10px;
+      left: 0;
+      width: 100%;
+      text-align: center;
     }
   }
 </style>
