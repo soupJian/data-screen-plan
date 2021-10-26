7735d5f2fdae0144a1b5726d6d6b8fbb30862bc1
soupjian <soupjian@163.com>
2021-10-26 11:06:35 +0800

    feat:减少打包后体积

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ?'./' : '/',
+  productionSourceMap: false, // 生产打包时不输出map文件，增加打包速度
   configureWebpack: {
     plugins: [
       new CopyWebpackPlugin([
